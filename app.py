369f9549078e43feacf6871c495579dd5dae4026
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-04-04 10:45:38 -0300

    FIX:  informações de login, incluí o metodo de busca e busca de paciente

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, requests;
 
+# -------------------- LOGIN --------------------
 
 urlLogin = "https://saude.sulamericaseguros.com.br/prestador/login/?accessError=2"
 
@@ -9,14 +10,46 @@ credenciais = {
     'passwrd': "837543"
 }
 
-session = requests.Session()
+requisicao = requests.Session()
 
-response = session.post(login_url, dados=credenciais)
+response = requisicao.post(urlLogin, dados=credenciais)
 
 if response.status_code == 200:
     print("Login realizado com sucesso!")
 else:
     print("Não foi possível realizar o login. Status:", response.status_code)
+
+
+# ------------------- SEARCH --------------------
+
+urlSearch = "https://saude.sulamericaseguros.com.br/prestador/servicos-medicos/contas-medicas/faturamento-tiss-3/faturamento/guia-de-consulta/"
+
+response = requisicao.get(urlSearch)
+
+if response.status_code == 200:
+    print("Esta é a página pra buscar informações!")
+else:
+    print("Não foi possível chegar a página e busca. Status:", response.status_code)
+
+
+# -------------------- SEARCH CLIENT ID ------------------------
+
+
+urlPaciente = "https://saude.sulamericaseguros.com.br/lumis/portal/controller/html/PortalRequestParametersControllerHtml.jsp"
+
+carteira = {
+    'codPaciente': "55788888485177660015"
+}
+
+response = requisicao.post(urlPaciente, json=carteira)
+
+if response.status_code == 200:
+    print("Paciente localizado na base de dados! Dados:")
+    print(response.json())
+else:
+    print("Paciente não encontrado! Status:", response.status_code)
+
+
 
 
 
