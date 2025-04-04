ee72ef786e4adca979932168d03d9b0276c7e184
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-04-04 09:59:05 -0300

    ADD: lógica post do login

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
-from flask import Flask
+from flask import Flask, requests;
+
+
+urlLogin = "https://saude.sulamericaseguros.com.br/prestador/login/?accessError=2"
+
+credenciais = {
+    'codRef': "100000009361",
+    'user': "master",
+    'passwrd': "837543"
+}
+
+session = requests.Session()
+
+response = session.post(login_url, dados=credenciais)
+
+if response.status_code == 200:
+    print("Login realizado com sucesso!")
+else:
+    print("Não foi possível realizar o login. Status:", response.status_code)
+
+
+
+
 
 app = Flask(__name__)
 
