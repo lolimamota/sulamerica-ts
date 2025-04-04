612b10ae2aa4d390ced50038b06c0d498b869c01
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-04-04 11:15:14 -0300

    ADD: library .env

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
-from flask import Flask, requests;
+from flask import Flask;
+
+import requests;
+
+import os;
+
+from flask_cors import CORS;
+
+from dotenv import load_dotenv;
 
 # -------------------- LOGIN --------------------
 
 urlLogin = "https://saude.sulamericaseguros.com.br/prestador/login/?accessError=2"
 
+
+load_dotenv()
+
 credenciais = {
-    'codRef': "100000009361",
-    'user': "master",
-    'passwrd': "837543"
+    'codRef': os.getenv("COD_REF"),
+    'user': os.getenv("USER"),
+    'passwrd': os.getenv("PASSWORD")
 }
 
 requisicao = requests.Session()
 
-response = requisicao.post(urlLogin, dados=credenciais)
+response = requisicao.post(urlLogin, data=credenciais)
 
-if response.status_code == 200:
+if response.status_code == 200 and "sucesso" in response.text.lower():
     print("Login realizado com sucesso!")
 else:
-    print("Não foi possível realizar o login. Status:", response.status_code)
+    print("Não foi possível realizar o login.")
+
 
 
 # ------------------- SEARCH --------------------
@@ -51,10 +63,11 @@ else:
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
     return '<h1>API de automação de login</h1><hr>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
